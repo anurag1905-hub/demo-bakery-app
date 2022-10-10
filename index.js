8a34466a97b45b537950be153f6f7306dab13d13
anurag1905-hub <anuragharsh626@gmail.com>
2022-10-10 17:41:33 +0530

    Add Code

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+
+const router = require('./routes');
+
+app.use(express.urlencoded());
+app.use(express.static('./assets'));
+
+
+// set up view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
+app.use('/',require('./routes'));
+
+app.listen(port,function(err){
+    if(err){
+        console.log('Error in starting the server',err);
+    }
+    else{
+        console.log('Server is running successfully at port number ',port);
+    }
+});
